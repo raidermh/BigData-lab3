5734d5967cdf3e620cfcc2af12365ab7306cf95a
raidermh <raidermh@gmail.com>
2021-04-30 14:12:59 +0300

    complete runApp bash script

---
+++ runApp_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $# -ne 4 ]] ; then
+    echo 'Please! Input 4 args: 1 - OpenApi Token 2 - Ticker Name 3 - Interval Time 4 - Boolean Is Sandbox? '
+    exit 1
+fi
+
+# Build
+cd ~/BigData-lab3
+mvn clean package
+
+# Run App
+java -jar ~/BigData-lab3/example/target/openapi-java-sdk-example-0.6-SNAPSHOT.jar $1 $2 $3 $4
+rm ~/BigData-lab3/logs/appLogs.lck
